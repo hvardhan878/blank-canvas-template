6e8e0a67565e9f055e65b05cd7ec579ef538e0f1
hvardhan878 <hvardhan878@gmail.com>
2025-04-10 08:58:14 +0100

    Updated the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 3000,
+    allowedHosts: [
+      '.modal.host'  // This will match any subdomain of modal.host
+    ]
   },
   plugins: [
     react(),
